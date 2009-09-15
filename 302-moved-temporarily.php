ef0a228d23906a8928320c5f7c954ca6305e9a86
bigfoot13442 <bigfoot13442@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-15 18:03:03 +0000

    first version of plugin
    git-svn-id: https://plugins.svn.wordpress.org/302-moved-temporarily/trunk@155057 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 302-moved-temporarily.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: 302 Moved Temporarily
+Plugin URI: 
+Description: Redirects any unauthenticated users to another address by sending them a '302 Moved Temporarily' message.
+Author: Jordan Patterson
+Version: 0.1
+Author URI: http://bluetreemedia.ca
+License: GPL 2.0, @see http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+function create_redirect_header($status_header, $header, $text, $protocol) {
+	if ( !is_user_logged_in() ) {
+		return "$protocol 302 Moved Temporarily";
+	}
+}
+
+function redirect_to() {
+	if ( !is_user_logged_in() ) {
+		header('location: '.get_option('where_to_send'));
+		die;
+	}
+}
+
+function redirected_feed() {
+	if ( !is_user_logged_in() ) {
+		die('<?xml version="1.0" encoding="UTF-8"?>'.
+			'<status>Service temporarialy unavailable</status>');
+	}
+}
+
+function feed_when_redirected() {
+	$feeds = array ('rdf', 'rss', 'rss2', 'atom');
+	foreach ($feeds as $feed) {
+		add_action('do_feed_'.$feed, 'redirected_feed', 1, 1);		
+	}
+}
+
+function add_redirect_menu(){
+	register_setting('redirect_menu_options','where_to_send','redirect_validation');
+	add_options_page('redirect_options_menu','302 Moved Temporarily',8,'redirectoptions','create_redirect_menu');
+}
+
+function create_redirect_menu(){
+	?>
+	<div class="wrap">
+		<h2>302 Moved Temporarily</h2>
+		<form method="post" action="options.php">
+		<table class="form-table">
+			<tr valign="top">
+			<th scope="row">Redirect URL</th>
+			<td><input type="text" size="50" name="where_to_send" value="<?php echo get_option('where_to_send'); ?>" /></td>
+			</tr>
+		</table>
+		<?php settings_fields( 'redirect_menu_options' ); ?>
+		<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+		</p>
+		</form>
+	</div>
+	<?php
+}
+
+function redirect_validation($input){
+	$input=filter_var($input,FILTER_SANITIZE_URL);
+	if(!preg_match('%^(?:http://|https://|mailto://)%',$input)){
+		$input='http://'.$input;
+	}
+	if($input&&filter_var($input,FILTER_VALIDATE_URL)){
+		return $input;
+	}else{
+		return false;
+	}
+}
+add_action('admin_menu','add_redirect_menu');
+if (function_exists('add_filter') ){
+	add_filter('status_header', 'create_redirect_header', 10, 4);
+	add_action('get_header', 'redirect_to');
+	feed_when_redirected();
+}else{
+	die('Unauthorized');
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,27 @@
+=== 302 Moved Temporarily ===
+Contributors: Blue Tree Media
+Tags: admin, maintenance, under construction, plugin, development, 302, temporary, mode, moved, site changes, redirect
+Requires at least: 2.7
+Tested up to: 2.8
+Stable tag: 1.0
+
+Redirects any unauthenticated users to another address and sends them a '302 Moved Temporarily' status.
+
+== Description ==
+
+Redirects any unauthenticated users to another address and sends them a '302 Moved Temporarily' status.
+Ideal for sites that are under development especially if the site is being moved or currently has an older version.
+Adds an item to the 'Options' menu on the admin panel where you can enter the site you want to redirect users to.
+
+== Installation ==
+
+1. Unzip `302-moved-temporarily.zip` and upload the contents files to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Set the page to redirect to under 'Options'.
+
+== Frequently Asked Questions ==
+
+=What version of PHP does this work with?=
+302 Moved Temporarily requires PHP 5.2 to work properly
+
+==Changelog==
